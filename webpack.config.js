172f15c963a6245b270aead4c284e1feccff459e
Roman <romanbond1111@gmail.com>
2025-05-29 19:14:09 +0200

    ready mb

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development', // Явно указываем режим
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   module: {
     rules: [
@@ -17,36 +15,25 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            configFile: path.resolve(__dirname, 'babel.config.json') // Явное указание конфига
-          }
-        }
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[hash][ext][query]'
-        }
-      }
-    ]
+        test: /\.(png|jpg|gif|svg)$/,
+        type: 'asset/resource',  
+      },
+    ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      inject: 'body'
-    })
-  ],
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
     },
-    compress: true,
-    port: 9000,
     hot: true,
-    open: true
-  }
+    open: true,
+  },
 };
